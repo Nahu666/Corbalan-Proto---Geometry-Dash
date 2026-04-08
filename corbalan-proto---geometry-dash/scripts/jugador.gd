514b4ea1b33968d25a3459aa2038646218ef54fc
Nahu <nahucorbalan06@gmail.com>
2026-04-08 11:29:48 -0300

    desarrollo de nivel de prueba y cambio de assets

--- corbalan-proto---geometry-dash/scripts/jugador.gd
+++ corbalan-proto---geometry-dash/scripts/jugador.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 35500
-const JUMP_VELOCITY = -1000
+const JUMP_VELOCITY = -1450
 
 var gravity = 4100
 
 func _physics_process(delta):
 	# GRAVEDAD
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# SALTO
 	if Input.is_action_pressed("salto") and is_on_floor():
